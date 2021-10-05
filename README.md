d6db809548d17aead62fd3db6f6da598a8e2619b
Daniel Varela <80353962+danielvarelaaa@users.noreply.github.com>
2021-10-05 20:46:28 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# myinstall-achlinux
+
+bfhbsvfdbabdkvbvbka
+
---
+++ codecs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## codecs...
+
+# Audio
+sudo pacman -S a52dec faac faad2 flac lame libdca libmad libvorbis wavpack
+
+# Imagem
+sudo pacman -S jasper
+
+# Video
+sudo pacman -S libdv libmpeg2 libtheora libxv x264 xvidcore gst-libav
+
