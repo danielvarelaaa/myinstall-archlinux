c6c9d12c7765650dc08e6f8870b3ffd27a654b73
Daniel Varela <80353962+danielvarelaaa@users.noreply.github.com>
2021-10-28 16:56:50 -0300

    Update other-repositories.sh

--- other-repositories.sh
+++ other-repositories.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+## atualização
+sudo pacman -Syu
+
+## instalação "yay"
+mkdir /home/$USER/Downloads/AUR
+cd /home/$USER/Downloads/AUR && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+## instala o "snap"
+cd /home/$USER/Downloads/AUR && git clone https://aur.archlinux.org/snapd.git && cd snapd && makepkg -si && sudo systemctl enable --now snapd.socket && sudo ln -s /var/lib/snapd/snap /snap
